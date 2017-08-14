426edd7878495fb7e2a748d487ceb6bc261914ad
Mounir Ybanez <rinuom91@gmail.com>
2017-08-14 11:15:29 +0800

    Refactored build script.

--- build.sh
+++ build.sh
@@ -1,31 +1,79 @@
-rm -rf build
-mkdir build
-mkdir build/auth
-mkdir build/data
-mkdir build/storage
-mkdir build/storage/file
-mkdir build/storage/file/song
-mkdir build/storage/file/user
-mkdir build/storage/file/album
-mkdir build/storage/file/artist
-mkdir build/storage/file/playlist
+build_auth_server() { 
+    echo "Building auth server"
 
-echo "Building auth server"
-cd auth/cli/
-go build
-mv cli ../../build/auth/auth
-cp config.json ../../build/auth/config.json
+    if [ ! -d "build/auth" ]; then
+        mkdir build/auth
+    fi
 
-echo "Building data server"
-cd ../../data/cli
-go build
-mv cli ../../build/data/data
-cp config.json ../../build/data/config.json
+    go build ./auth/cli
+    mv cli ./build/auth/auth
+    cp ./auth/cli/config.json ./build/auth/config.json
+}
 
-echo "Building storage server"
-cd ../../storage/cli
-go build
-mv cli ../../build/storage/storage
-cp config.json ../../build/storage/config.json
+build_data_server() {
+    echo "Building data server"
+    
+    if [ ! -d "build/data" ]; then
+        mkdir build/data
+    fi
+
+    go build ./data/cli
+    mv cli ./build/data/data
+    cp ./data/cli/config.json ./build/data/config.json
+}
+
+build_storage_server() {
+    echo "Building storage server"
+
+    if [ ! -d "build/storage" ]; then
+        mkdir build/storage
+    fi
+
+    if [ ! -d "build/storage/file" ]; then
+        mkdir build/storage/file
+    fi
+
+    if [ ! -d "build/storage/file/song" ]; then
+        mkdir build/storage/file/song
+    fi
+
+    if [ ! -d "build/storage/file/user" ]; then
+        mkdir build/storage/file/user
+    fi
+
+    if [ ! -d "build/storage/file/album" ]; then
+        mkdir build/storage/file/album
+    fi
+
+    if [ ! -d "build/storage/file/artist" ]; then
+        mkdir build/storage/file/artist
+    fi
+
+    if [ ! -d "build/storage/file/playlist" ]; then
+        mkdir build/storage/file/playlist
+    fi
+
+    go build ./storage/cli
+    mv cli ./build/storage/storage
+    cp ./storage/cli/config.json ./build/storage/config.json
+}
+
+build_all_servers() {
+    build_auth_server
+    build_data_server
+    build_storage_server
+}
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+
+case $1 in
+    "") build_all_servers;;
+    "auth") build_auth_server;;
+    "data") build_data_server;;
+    "storage") build_storage_server;;
+esac
 
 echo "Finished"
