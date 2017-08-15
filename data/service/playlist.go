097065e1fc614a8011ad5143b391e1fad6317634
Mounir Ybanez <rinuom91@gmail.com>
2017-08-15 10:12:55 +0800

    Set playlist key to empty on updating.

--- data/service/playlist.go
+++ data/service/playlist.go
@@ -1,8 +1,9 @@
 package service
 
 import (
-	"github.com/mownier/duyog/data/store"
 	"net/http"
+
+	"github.com/mownier/duyog/data/store"
 
 	"github.com/gorilla/mux"
 )
@@ -87,6 +88,8 @@ func (p playlist) update(w http.ResponseWriter, r *http.Request) {
 		writeRespErr(w, r, p.ResponseWriter, err)
 		return
 	}
+
+	playlist.Key = ""
 
 	marshalResponse(w, r, p.ResponseWriter, playlist)
 }
