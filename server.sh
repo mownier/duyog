8ceeadef560f2d21c650441c0743d40b211d4c52
Mounir Ybanez <rinuom91@gmail.com>
2017-08-15 16:24:02 +0800

    Renamed to server.sh.
    - Handled killing of processes using pkill
    - Used PID file to kill the process

---
+++ server.sh
@@ -0,0 +1,116 @@
+AUTH_SERVER=./build/auth/auth
+AUTH_SERVER_PID=./build/auth/auth.pid
+AUTH_SERVER_LOG=./build/auth/auth.log
+AUTH_SERVER_CNF=./build/auth/config.json
+
+DATA_SERVER=./build/data/data
+DATA_SERVER_PID=./build/data/data.pid
+DATA_SERVER_LOG=./build/data/data.log
+DATA_SERVER_CNF=./build/data/config.json
+
+STORAGE_SERVER=./build/storage/storage
+STORAGE_SERVER_PID=./build/storage/storage.pid
+STORAGE_SERVER_LOG=./build/storage/storage.log
+STORAGE_SERVER_CNF=./build/storage/config.json
+
+start_auth_server() {
+    if [ -f $AUTH_SERVER_PID ]; then
+        echo "already running auth server"
+    else
+        $AUTH_SERVER start -config $AUTH_SERVER_CNF >> $AUTH_SERVER_LOG 2<&1 & auth_pid=$!
+        echo "auth server started"
+        echo $auth_pid > $AUTH_SERVER_PID
+    fi
+}
+
+start_data_server() {
+    if [ -f $DATA_SERVER_PID ]; then
+        echo "already running data server"
+    else
+        if [ ! -f $AUTH_SERVER_PID ]; then
+            echo "no auth server running"
+        else
+            $DATA_SERVER start -config $DATA_SERVER_CNF >> $DATA_SERVER_LOG 2<&1 & data_pid=$!
+            echo "data server started"
+            echo $data_pid > $DATA_SERVER_PID
+        fi
+    fi
+}
+
+start_storage_server() {
+    if [ -f $STORAGE_SERVER_PID ]; then
+        echo "already running storage server"
+    else
+        if [ ! -f $AUTH_SERVER_PID ]; then
+            echo "no auth server running"
+        else
+            if [ ! -f $DATA_SERVER_PID ]; then
+                echo "no data server running"
+            else
+                $STORAGE_SERVER start -config $STORAGE_SERVER_CNF >> $STORAGE_SERVER_LOG 2<&1 & storage_pid=$!
+                echo "storage server started"
+                echo $storage_pid > $STORAGE_SERVER_PID
+            fi
+        fi
+    fi
+}
+
+stop_auth_server() {
+    if [ ! -f $AUTH_SERVER_PID ]; then
+        echo "no auth server process running"
+    else
+        pkill -F $AUTH_SERVER_PID
+        rm $AUTH_SERVER_PID
+        echo "auth server stopped"
+    fi
+}
+
+stop_data_server() {
+    if [ ! -f $DATA_SERVER_PID ]; then
+        echo "no data server process running"
+    else
+        pkill -F $DATA_SERVER_PID
+        rm $DATA_SERVER_PID
+        echo "data server stopped"
+    fi
+}
+
+stop_storage_server() {
+    if [ ! -f $STORAGE_SERVER_PID ]; then
+        echo "no storage server process running"
+    else
+        pkill -F $STORAGE_SERVER_PID
+        rm $STORAGE_SERVER_PID
+        echo "storage server stopped"
+    fi
+}
+
+start_all_servers() {
+    start_auth_server &&
+    start_data_server &&
+    start_storage_server
+}
+
+stop_all_servers() {
+    stop_storage_server &&
+    stop_data_server &&
+    stop_auth_server
+}
+
+case $1 in
+    "start") 
+        case $2 in
+            "") start_all_servers;;
+            "auth") start_auth_server;;
+            "data") start_data_server;;
+            "storage") start_storage_server;;
+        esac;;
+    
+    "stop")
+        case $2 in
+            "") stop_all_servers;;
+            "auth") stop_auth_server;;
+            "data") stop_data_server;;
+            "storage") stop_storage_server;;
+        esac;;
+esac
