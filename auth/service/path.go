be08ae2effb4ce394f659d91ae2fcd30810c905d
Mounir Ybanez <rinuom91@gmail.com>
2017-08-17 11:47:31 +0800

    Added documentation on package service and pathCode.

--- auth/service/path.go
+++ auth/service/path.go
@@ -1,21 +1,35 @@
+// Package service provides the authentication server with
+// the HTTP handler functions for token and user resources.
+// Also, it provides a microservice using JSON RPC to verify
+// whether the access token is valid or not.
 package service
 
+// pathCode identifies an HTTP handler function
 type pathCode int
 
 const (
-	// pathCodeUserSignIn code
+	// pathCodeUserSignIn ties with the HTTP
+	// handler function on signing in a user
 	pathCodeUserSignIn pathCode = 1000
 
-	// pathCodeUserSignUp code
+	// pathCodeUserSignUp ties with the HTTP
+	// handler function on signing up a user
 	pathCodeUserSignUp pathCode = 1001
 
-	// pathCodeUserChangePass code
+	// pathCodeUserChangePass ties with the HTTP
+	// handler function on changing user password
 	pathCodeUserChangePass pathCode = 1002
 
-	// pathCodeTokenRefresh code
+	// pathCodeTokenRefresh ties with the HTTP
+	// handler function on refreshing an access token
 	pathCodeTokenRefresh pathCode = 2000
 )
 
+// forbiddenPath holds the information whether a certain
+// HTTP handler function forbidden or not.
+// This will be overruled if the role of a client is admin.
+// Client's role will be retrieved by decoding the
+// access token which is a JSON Web Token.
 var forbiddenPath = map[pathCode]bool{
 	pathCodeUserSignIn:     false,
 	pathCodeUserSignUp:     false,
@@ -24,6 +38,8 @@ var forbiddenPath = map[pathCode]bool{
 	pathCodeTokenRefresh: false,
 }
 
+// isForbidden determines whether a particular pathCode
+// is forbidden or not.
 func isForbidden(c pathCode) bool {
 	return forbiddenPath[c]
 }
